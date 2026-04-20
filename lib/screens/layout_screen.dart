1cc49f2a1a4e83fee19af829c47ebd84360f0bb4
Lars Liverød <lars.liverod@knowit.no>
2026-04-20 11:00:56 +0200

    Initial IOS config

--- lib/screens/layout_screen.dart
+++ lib/screens/layout_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app/util/app_colors.dart';
 import 'package:app/widgets/screen_scaffold.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class LayoutScreen extends StatelessWidget {
